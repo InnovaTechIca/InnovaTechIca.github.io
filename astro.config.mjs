1c91a496f5e19e26589ffe3707348b82a963749e
MiguelVivar <miguelvivarfarfan@gmail.com>
2025-05-11 23:54:58 -0500

    actu

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://innovatech-ica.com', // Agrega la URL de tu sitio web aquí
   vite: {
     plugins: [tailwindcss()]
   }
